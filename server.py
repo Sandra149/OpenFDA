46464bf1b49a22834dc4ab02fc1f4342e3e9e4ac
Sandra149 <sandratorres149@gmail.com>
2017-04-07 07:57:53 +0200

    Update server.py

--- server.py
+++ server.py
@@ -13,8 +13,3 @@ Handler = web.testHTTPRequestHandler
 httpd = socketserver.TCPServer(("", PORT), Handler)
 print("serving at port", PORT)
 httpd.serve_forever()
-'''
-except KeyboardInterrupt:
-    print('CTRL+C received, shutting down server')
-    server.socket.close()'''
-
