08aa9b33d31c4c4bafd15ac40b6a483a3d54b042
Sandra149 <sandratorres149@gmail.com>
2017-05-01 14:18:08 +0200

    Update web.py
    Addition and comment code

--- web.py
+++ web.py
@@ -18,64 +18,97 @@
 import http.client
 import http.server
 import json
+import food
 
-#ejecutar con pyhton3
-
+'''
+ Clase que se conectará a la API.
+'''
 class OpenFDAClient():
 
     OPENFDA_API_URL = "api.fda.gov"
-    OPENFDA_API_EVENT='/drug/event.json'
+    OPENFDA_API_EVENT = '/drug/event.json'
 
-    def get_response(self, query = '',limite = ''):
+    '''
+     Función que devuelve la respuesta sin parsear.
+    '''
+    def get_response(self, query = '', limit = ''):
+
         conn = http.client.HTTPSConnection(self.OPENFDA_API_URL)
 
         if len(query) == 0:
-            limite = '?limit='+str(limite)
+            limit = '?limit=' + str(limit)
         else:
-            limite = '&limit=10'
+            limit = '&limit=10'
         
-        
-        print (self.OPENFDA_API_EVENT +limite)
+        print (self.OPENFDA_API_EVENT + limit)
 
-        conn.request("GET", self.OPENFDA_API_EVENT + query+limite)
+        conn.request("GET", self.OPENFDA_API_EVENT + query + limit)
 
         response = conn.getresponse()
         print (response.status, response.reason)
 
         return response.read().decode('utf8')    
 
+'''
+ Clase que parseará los JSONs.
+'''
 class OpenFDAParser():
     
-    def get_drugs_from_medicines(self,medicines_list):
-        drugs=[]
+    '''
+     Función que devuelve la lista de 
+    '''
+    def get_drugs_from_medicines(self, medicines_list):
+
+        drugs = []
         for medicine in medicines_list:
-            drugs+= [medicine['patient']['drug'][0]['medicinalproduct']]
+            drugs += [medicine['patient']['drug'][0]['medicinalproduct']]
 
         return drugs
 
-    def get_companynumb(self,medicines_list):  
-        number_list=[]
+    '''
+     Función que devuelve la lista de 
+    '''
+    def get_companynumb(self, medicines_list):  
+
+        number_list = []
         for medicine in medicines_list:
             number_list += [medicine['companynumb']]
+
         return number_list
 
-    def get_company_from_events(self,events):
-        drugs=[]
+    '''
+    Se llama get company pero devuelve drugs
+    '''
+    def get_company_from_events(self, events):
+
+        drugs = []
         for event in events:
             drugs += [event['companynumb']]
+            
         return drugs
 
-    def get_patient_sex(self,events):
-        gender=[]
+    '''
+     Función que devuelve la lista de 
+    '''
+    def get_patient_sex(self, events):
+
+        gender = []
         for event in events:
             gender += [event['patient']['patientsex']]
+            
         return gender
 
+'''
+ Clase que forma los distintos HTMLs.
+'''
 class OpenFDAHTML():
 
-    def get_html(self,drugs):
+    '''
+     Función que devuelve la página con una lista de elementos.
+    '''
+    def get_html(self, elems):
 
-        drugs_html = '''
+        html = '''
         <html>
             <head>
                 <title>OpenFDA Cool App</title>
@@ -83,171 +116,217 @@ class OpenFDAHTML():
             <body>
                 <ol>
                 '''
-        for drug in drugs:
-            drugs_html+= '<li>'+drug+'</li>\n'
+        for elem in elems:
+            html += '<li>' + elem + '</li>\n'
 
-        drugs_html+= '''
+        html += '''
                 </ol>
             </body>
         </html>
         '''
-        return drugs_html
+        return html
 
+    '''
+     Función que devuelve la página principal.
+    '''
     def get_main_page(self):
 
-        with open ('codigo_html.html','r') as f:
+        with open ('codigo_html.html', 'r') as f:
             html = f.read()
             
         return html
 
+    '''
+     Función que devuelve la página de error.
+    '''
     def get_error_page(self):
 
-        with open ('codigo_html_error.html','r') as f:
-
+        with open ('codigo_html_error.html', 'r') as f:
             html = ''
             for line in f:
-                html+= line
+                html += line
 
             return html
     
 
 class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
     
-    
     SEARCH_MEDICINE = "?search=patient.drug.medicinalproduct:"
     SEARCH_COMPANY = "?search=companynumb"
-
-    def pass_limit(self,path):
+    FILE_NAME = 'openfda.log'
+    DRUG = 'medicamentos'
+    COMPANY = 'compania'
+    
+    '''
+     Función que devuelve el límite introducido por el usuario.
+    '''
+    def get_limit(self, path):
 
         limite_usuario = path.split('=')[1]
-        return str(limite_usuario)
+        return limite_usuario
 
-    def correct_server_answer(self):
+    '''
+     Función que establece las cabeceras HTTPs con el código recibido.
+    '''
+    def set_headers(self, http_code):
 
-        self.send_response(200)
-        self.send_header('Content-type','text/html')
+        self.send_response(http_code)
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
     
+    '''
+     Función que comprueba que el límite es un número.
+    '''
     def is_query_ok(self, query):
+
         if 'limit' in query:
             try:
-                limit = self.path.split('=')[1]
-
-                limit = int(limit)
+                int(self.get_limit(query))
 
             except ValueError:
                 return False
 
         return True
 
+    '''
+     Función que almacena lo que ha buscado el usuario en un fichero.
+    '''
+    def store_info(self, list_response, file_name, search_type, search = None):
+
+        with open (file_name, 'a') as file:
+            if search == None:
+                file.write('El usuario ha buscado ' + search_type + ':\n')
+            else:
+                file.write('El usuario ha buscado ' + search_type + ' ' + search + ':\n')
+                
+            for words in list_response:
+                file.write(words + '\n')
+
+            file.write('\n')
+
     def do_GET(self):
                 
         print(self.path)
 
         correct = self.is_query_ok(self.path)
+
         client = OpenFDAClient()
         parser = OpenFDAParser()
         html = OpenFDAHTML()
 
-        if not correct:
-            self.send_response(404)
-            self.send_header('Content-Type', 'text/html')
-            self.end_headers()
-            
-            html = html.get_error_page()
+        food_client = food.OpenFDAClient()
 
-            self.wfile.write(bytes(html, 'utf8'))
+        if not correct:
+
+            self.set_headers(404)
+            
+            html_page = html.get_error_page()
+
+            self.wfile.write(bytes(html_page, 'utf8'))
 
             return         
 
-        if self.path=='/':
-            
-            self.correct_server_answer()
-        
-            html = html.get_main_page()
+        if self.path == '/':
 
-            self.wfile.write(bytes(html, "utf8")) 
+            self.set_headers(200)
+        
+            html_page = html.get_main_page()
+
+            self.wfile.write(bytes(html_page, "utf8")) 
 
         elif '/listDrugs?limit=' in self.path:
 
-            self.correct_server_answer()
+            self.set_headers(200)
 
-            query=''
-            jsons=client.get_response(query,self.pass_limit(self.path))
-            medicines_list=json.loads(jsons)['results']
+            query = ''
+            jsons = client.get_response(query, self.get_limit(self.path))
+            medicines_list = json.loads(jsons)['results']
 
-            drugs= parser.get_drugs_from_medicines(medicines_list)
+            drugs = parser.get_drugs_from_medicines(medicines_list)
 
-            html = html.get_html(drugs)
-            self.wfile.write(bytes(html, "utf8"))
+            html_page = html.get_html(drugs)
+
+            self.wfile.write(bytes(html_page, 'utf8'))
 
         elif '/searchDrug?' in self.path:
 
-            self.correct_server_answer()
+            self.set_headers(200)
 
             limite = 10
-
-            medicamento_usuario=self.path.split('=')[1]
+            medicamento_usuario = self.path.split('=')[1]
             
-            jsons = client.get_response(self.SEARCH_MEDICINE+medicamento_usuario,limite)
+            jsons = client.get_response(self.SEARCH_MEDICINE + medicamento_usuario, limite)
             medicines_list = json.loads(jsons)['results']
 
             company_numb = parser.get_companynumb(medicines_list)
 
-            html = html.get_html(company_numb)
-            self.wfile.write(bytes(html, "utf8"))
+            self.store_info(company_numb, self.FILE_NAME, self.DRUG, medicamento_usuario)
+
+            html_page = html.get_html(company_numb)
+
+            self.wfile.write(bytes(html_page, 'utf8'))
 
         elif '/listCompanies?limit=' in self.path:
 
-            self.correct_server_answer()
+            self.set_headers(200)
 
-            query=''
-
-            jsons=client.get_response(query, self.pass_limit(self.path))
-            company_list=json.loads(jsons)['results']
+            query = ''
+            jsons = client.get_response(query, self.get_limit(self.path))
+            company_list = json.loads(jsons)['results']
 
             drugs = parser.get_company_from_events(company_list)
 
-            html = html.get_html(drugs)
-            self.wfile.write(bytes(html, "utf8"))
+            html_page = html.get_html(drugs)
+
+            self.wfile.write(bytes(html_page, 'utf8'))
 
         elif '/searchCompany?' in self.path:
 
-            self.correct_server_answer()
+            self.set_headers(200)
 
-            limite=10
-
+            limite = 10
             company_numb = self.path.split('=')[1]
 
-            print(self.path)
-
-            jsons = client.get_response(self.SEARCH_COMPANY.replace('companynumb', company_numb),limite)
+            jsons = client.get_response(self.SEARCH_COMPANY.replace('companynumb', company_numb), limite)
             company = json.loads(jsons)['results']
 
-            company_numb= parser.get_drugs_from_medicines(company)
-            html = html.get_html(company_numb)
-            self.wfile.write(bytes(html, "utf8"))
+            company_numb = parser.get_drugs_from_medicines(company)
+            html_page = html.get_html(company_numb)
+
+            self.wfile.write(bytes(html_page, 'utf8'))
         
         elif '/listGender?limit=' in self.path:
-            
-	self.correct_server_answer()
+
+            self.set_headers(200)
 
             query = ''
-
-            jsons = client.get_response(query,self.pass_limit(self.path))
+            jsons = client.get_response(query, self.get_limit(self.path))
             gender_list = json.loads(jsons)['results']
 
             drugs = parser.get_patient_sex(gender_list)
 
-            html = html.get_html(drugs)
-            self.wfile.write(bytes(html, "utf8"))
-        else:
+            html_page = html.get_html(drugs)
 
-            self.send_response(404)
-            self.send_header('Content-type','text/html')
+            self.wfile.write(bytes(html_page, 'utf8'))
+
+        elif '/secret' in self.path:
+
+            self.send_response(401)
+            self.send_header('WWW-Authenticate', 'Basic realm="My Realm"')
             self.end_headers()
 
-            html = html.get_error_page()
-            self.wfile.write(bytes(html, "utf8"))
+        elif '/redirect' in self.path:
+
+            self.send_response(302)
+            self.send_header('Location', 'http://localhost:8000/')
+            self.end_headers()
+            
+        else:
+
+            self.set_headers(404)
+
+            html_page = html.get_error_page()
+
+            self.wfile.write(bytes(html_page, 'utf8'))
 
         return
